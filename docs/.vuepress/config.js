9e7808ef7a7810aff7eca904aa52c85b595cda0d
王相博 <7685139+pythonistaWong@user.noreply.gitee.com>
2021-01-23 10:16:35 +0800

    add: config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-01-19 09:07:13
- * @LastEditTime: 2021-01-19 09:07:23
+ * @LastEditTime: 2021-01-23 10:16:16
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /vuepress-starter/docs/.vuepress/config.js
@@ -9,5 +9,30 @@
 
 module.exports = {
   title: 'Hello VuePress',
-  description: 'Just playing around'
+  description: 'Just playing around',
+  themeConfig: {
+    nav: [ // 导航栏配置
+      {
+        text: '个人主页',
+        link: '/accumulate/'
+      },
+      {
+        text: '个人其他',
+        link: '/algorithm/'
+      },
+      {
+        text: 'B站',
+        link: 'https://baidu.com'
+      }
+    ],
+    sidebar: 'auto', // 侧边栏配置
+    sidebarDepth: 2, // 侧边栏显示2级
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@alias': '/'
+      }
+    }
+  }
 }
