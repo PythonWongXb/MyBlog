c168e64f74065ea1b258b412221005f3b8ea3365
王相博 <7685139+pythonistaWong@user.noreply.gitee.com>
2021-01-23 10:07:24 +0800

    Revert "add: basic files constructor"
    This reverts commit 61d149c6f3e68212faa11115af3af59a8ef4641d.

--- docs/.vuepress/theme/layout.vue
+++
@@ -1,8 +0,0 @@
-<!--
- * @Author: your name
- * @Date: 2021-01-23 09:39:03
- * @LastEditTime: 2021-01-23 09:39:06
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: /vuepress-starter/docs/.vuepress/theme/layout.vue
--->
