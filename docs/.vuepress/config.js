ddc46f2333592a1697902279cae5c45e777278b4
王相博 <7685139+pythonistaWong@user.noreply.gitee.com>
2021-01-23 09:10:38 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2021-01-19 09:07:13
+ * @LastEditTime: 2021-01-19 09:07:23
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /vuepress-starter/docs/.vuepress/config.js
+ */
+
+module.exports = {
+  title: 'Hello VuePress',
+  description: 'Just playing around'
+}
