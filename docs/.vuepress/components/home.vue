2c3549c5cc00acfbd3a771f64a6e83add4b6606a
王相博 <7685139+pythonistaWong@user.noreply.gitee.com>
2021-01-25 09:18:33 +0800

    add: style

--- docs/.vuepress/components/home.vue
+++ docs/.vuepress/components/home.vue
@@ -1,14 +1,31 @@
 <!--
  * @Author: your name
  * @Date: 2021-01-24 20:09:22
- * @LastEditTime: 2021-01-24 21:26:28
+ * @LastEditTime: 2021-01-25 09:08:29
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /vuepress-starter/docs/.vuepress/components/home.vue
 -->
 <style scoped lang="sass">
 .app_container
-
+  background-size: cover
+  background-repeat: no-repeat
+  background-image: url(http://www.ruanyifeng.com/images_pub/pub_0.png)
+  background-attachment: fixed
+  -webkit-background-size: cover
+  -moz-background-size: cover
+  -o-background-size: cover
+  background-size: cover
+  background-position: 50% 50%
+  margin: 0
+  padding: 0
+  width: 100%
+  height: 100%
+  -webkit-transition: All 2s ease
+  -moz-transition: All 2s ease
+  -o-transition: All 2s ease
+  -ms-transition: All 2s ease
+  transition: All 2s ease
 </style>
 
 <template lang="pug">
