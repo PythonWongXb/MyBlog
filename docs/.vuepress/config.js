87f70ae755072399314d3a8767b3c47656007426
王相博 <7685139+pythonistaWong@user.noreply.gitee.com>
2021-02-09 20:51:05 +0800

    feature: Project construction

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-01-27 22:17:44
- * @LastEditTime: 2021-02-06 14:09:40
+ * @LastEditTime: 2021-02-09 20:47:23
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /vuepress-starter/docs/.vuepress/config.js
@@ -126,172 +126,232 @@ module.exports = {
             ]
           },
         ]
+      },
+      {
+        text: 'Projects',
+        items: [
+          {
+            text: 'VenueOnlineManageSystem',
+            link: '/Projects/VenueOnlineManageSystem/'
+          },
+        ]
       }
     ],
     sidebarDepth: 2,
     sidebar: {
-        '/JavaScript/': [
-          {
-            title: 'JavaScript基础',
-            path: '/JavaScript/#1',
-            children: [
-              {
-                title: '1',
-                path: '/JavaScript/one'
-              },
-              {
-                title: '2',
-                path: '/JavaScript/two'
-              },
-            ],
-          },
-          {
-            title: 'JavaScript中级',
-            path: '/JavaScript/#2',
-            children: [
-              '/'
-            ]
-          },
-          {
-            title: '算法',
-            path: '/JavaScript/algorithm',
-            children: [
-              {
-                title: 'fibo',
-                path: '/JavaScript/algorithm/fibonacci'
-              }, ]
-          },
-        ],
-        '/Golang/': [
-          {
-            title: 'Golang基础',
-            path: '/Golang/#1',
-            children: [
-              {
-                title: '1',
-                path: '/Golang/one'
-              },
-              {
-                title: '2',
-                path: '/Golang/two'
-              },
-            ],
-          },
-          {
-            title: 'JavaScript中级',
-            path: '/Golang/#2',
-            children: [
-              '/'
-            ]
-          },
-          {
-            title: '基础',
-            path: '/Golang/primary/',
-            children: [
-              {
-                title: '并发',
-                path: '/Golang/primary/并发'
-              }, ]
-          },
-        ],
-        '/PersonalStyle/Code/BadCodes/': [
-          {
-            title: '1-MysteriousName',
-            path: '/PersonalStyle/Code/BadCodes/1-MysteriousName',
-          },
-          {
-            title: '2-DuplicatedCode',
-            path: '/PersonalStyle/Code/BadCodes/2-DuplicatedCode',
-          },
-          {
-            title: '3-LongFunction',
-            path: '/PersonalStyle/Code/BadCodes/3-LongFunction',
-          },
-          {
-            title: '4-LongParameterList',
-            path: '/PersonalStyle/Code/BadCodes/4-LongParameterList',
-          },
-          {
-            title: '5-GlobalData',
-            path: '/PersonalStyle/Code/BadCodes/5-GlobalData',
-          },
-          {
-            title: '6-DivergentChange',
-            path: '/PersonalStyle/Code/BadCodes/6-DivergentChange',
-          },
-          {
-            title: '7-MutableData',
-            path: '/PersonalStyle/Code/BadCodes/7-MutableData',
-          },
-          {
-            title: '8-ShotgunSurgery',
-            path: '/PersonalStyle/Code/BadCodes/8-ShotgunSurgery',
-          },
-          {
-            title: '9-FeatureEnvy',
-            path: '/PersonalStyle/Code/BadCodes/9-FeatureEnvy',
-          },
-          {
-            title: '10-DataClumps',
-            path: '/PersonalStyle/Code/BadCodes/10-DataClumps',
-          },
-          {
-            title: '11-PimitiveObsession',
-            path: '/PersonalStyle/Code/BadCodes/11-PimitiveObsession',
-          },
-          {
-            title: '12-RepeatedSwitches',
-            path: '/PersonalStyle/Code/BadCodes/12-RepeatedSwitches',
-          },
-          {
-            title: '13-Loops',
-            path: '/PersonalStyle/Code/BadCodes/13-Loops',
-          },
-          {
-            title: '14-LazyElement',
-            path: '/PersonalStyle/Code/BadCodes/14-LazyElement',
-          },
-          {
-            title: '15-SpeculativeGenerality',
-            path: '/PersonalStyle/Code/BadCodes/15-SpeculativeGenerality',
-          },
-          {
-            title: '16-TemporaryField',
-            path: '/PersonalStyle/Code/BadCodes/16-TemporaryField',
-          },
-          {
-            title: '17-MessageChains',
-            path: '/PersonalStyle/Code/BadCodes/17-MessageChains',
-          },
-          {
-            title: '18-MiddleMan',
-            path: '/PersonalStyle/Code/BadCodes/18-MiddleMan',
-          },
-          {
-            title: '19-InsiderTrading',
-            path: '/PersonalStyle/Code/BadCodes/19-InsiderTrading',
-          },
-          {
-            title: '20-LargeClass',
-            path: '/PersonalStyle/Code/BadCodes/20-LargeClass',
-          },
-          {
-            title: '21-AlternativeClassWithDifferentInterfaces',
-            path: '/PersonalStyle/Code/BadCodes/21-AlternativeClassWithDifferentInterfaces',
-          },
-          {
-            title: '22-DataClass',
-            path: '/PersonalStyle/Code/BadCodes/22-DataClass',
-          },
-          {
-            title: '23-RefusedBequest',
-            path: '/PersonalStyle/Code/BadCodes/23-RefusedBequest',
-          },
-          {
-            title: '24-Comments',
-            path: '/PersonalStyle/Code/BadCodes/24-Comments',
-          },
-        ],
+      '/JavaScript/': [
+        {
+          title: 'JavaScript基础',
+          path: '/JavaScript/#1',
+          children: [
+            {
+              title: '1',
+              path: '/JavaScript/one'
+            },
+            {
+              title: '2',
+              path: '/JavaScript/two'
+            },
+          ],
+        },
+        {
+          title: 'JavaScript中级',
+          path: '/JavaScript/#2',
+          children: [
+            '/'
+          ]
+        },
+        {
+          title: '算法',
+          path: '/JavaScript/algorithm',
+          children: [
+            {
+              title: 'fibo',
+              path: '/JavaScript/algorithm/fibonacci'
+            }, ]
+        },
+      ],
+      '/Golang/': [
+        {
+          title: 'Golang基础',
+          path: '/Golang/#1',
+          children: [
+            {
+              title: '1',
+              path: '/Golang/one'
+            },
+            {
+              title: '2',
+              path: '/Golang/two'
+            },
+          ],
+        },
+        {
+          title: 'JavaScript中级',
+          path: '/Golang/#2',
+          children: [
+            '/'
+          ]
+        },
+        {
+          title: '基础',
+          path: '/Golang/primary/',
+          children: [
+            {
+              title: '并发',
+              path: '/Golang/primary/并发'
+            }, ]
+        },
+      ],
+      '/PersonalStyle/Code/BadCodes/': [
+        {
+          title: '1-MysteriousName',
+          path: '/PersonalStyle/Code/BadCodes/1-MysteriousName',
+        },
+        {
+          title: '2-DuplicatedCode',
+          path: '/PersonalStyle/Code/BadCodes/2-DuplicatedCode',
+        },
+        {
+          title: '3-LongFunction',
+          path: '/PersonalStyle/Code/BadCodes/3-LongFunction',
+        },
+        {
+          title: '4-LongParameterList',
+          path: '/PersonalStyle/Code/BadCodes/4-LongParameterList',
+        },
+        {
+          title: '5-GlobalData',
+          path: '/PersonalStyle/Code/BadCodes/5-GlobalData',
+        },
+        {
+          title: '6-DivergentChange',
+          path: '/PersonalStyle/Code/BadCodes/6-DivergentChange',
+        },
+        {
+          title: '7-MutableData',
+          path: '/PersonalStyle/Code/BadCodes/7-MutableData',
+        },
+        {
+          title: '8-ShotgunSurgery',
+          path: '/PersonalStyle/Code/BadCodes/8-ShotgunSurgery',
+        },
+        {
+          title: '9-FeatureEnvy',
+          path: '/PersonalStyle/Code/BadCodes/9-FeatureEnvy',
+        },
+        {
+          title: '10-DataClumps',
+          path: '/PersonalStyle/Code/BadCodes/10-DataClumps',
+        },
+        {
+          title: '11-PimitiveObsession',
+          path: '/PersonalStyle/Code/BadCodes/11-PimitiveObsession',
+        },
+        {
+          title: '12-RepeatedSwitches',
+          path: '/PersonalStyle/Code/BadCodes/12-RepeatedSwitches',
+        },
+        {
+          title: '13-Loops',
+          path: '/PersonalStyle/Code/BadCodes/13-Loops',
+        },
+        {
+          title: '14-LazyElement',
+          path: '/PersonalStyle/Code/BadCodes/14-LazyElement',
+        },
+        {
+          title: '15-SpeculativeGenerality',
+          path: '/PersonalStyle/Code/BadCodes/15-SpeculativeGenerality',
+        },
+        {
+          title: '16-TemporaryField',
+          path: '/PersonalStyle/Code/BadCodes/16-TemporaryField',
+        },
+        {
+          title: '17-MessageChains',
+          path: '/PersonalStyle/Code/BadCodes/17-MessageChains',
+        },
+        {
+          title: '18-MiddleMan',
+          path: '/PersonalStyle/Code/BadCodes/18-MiddleMan',
+        },
+        {
+          title: '19-InsiderTrading',
+          path: '/PersonalStyle/Code/BadCodes/19-InsiderTrading',
+        },
+        {
+          title: '20-LargeClass',
+          path: '/PersonalStyle/Code/BadCodes/20-LargeClass',
+        },
+        {
+          title: '21-AlternativeClassWithDifferentInterfaces',
+          path: '/PersonalStyle/Code/BadCodes/21-AlternativeClassWithDifferentInterfaces',
+        },
+        {
+          title: '22-DataClass',
+          path: '/PersonalStyle/Code/BadCodes/22-DataClass',
+        },
+        {
+          title: '23-RefusedBequest',
+          path: '/PersonalStyle/Code/BadCodes/23-RefusedBequest',
+        },
+        {
+          title: '24-Comments',
+          path: '/PersonalStyle/Code/BadCodes/24-Comments',
+        },
+      ],
+      '/Projects/VenueOnlineManageSystem/': [
+        {
+          title: '0-文件架构',
+          path: '/Projects/VenueOnlineManageSystem/0-FileConstruction',
+          children: [
+            {
+              title: '静态文件',
+              path: '/Projects/VenueOnlineManageSystem/0-FileConstruction/0-1-StaticFile/'
+            },
+            {
+              title: 'View架构',
+              path: '/Projects/VenueOnlineManageSystem/0-FileConstruction/0-2-ViewConstruction/'
+            },
+          ],
+        },
+        {
+          title: '1-组件架构',
+          path: '/Projects/VenueOnlineManageSystem/1-ComponentConstruction',
+          children: [
+            '/'
+          ]
+        },
+        {
+          title: '2-路由架构',
+          path: '/Projects/VenueOnlineManageSystem/2-VueRouterConstruction',
+          children: [
+            '/'
+          ]
+        },
+        {
+          title: '3-Axios架构',
+          path: '/Projects/VenueOnlineManageSystem/3-AxiosConstruction',
+          children: [
+            '/'
+          ]
+        },
+        {
+          title: '4-Vuex数据架构',
+          path: '/Projects/VenueOnlineManageSystem/4-VuexDataConstruction',
+          children: [
+            '/'
+          ]
+        },
+        {
+          title: '5-LocalStorage数据架构',
+          path: '/Projects/VenueOnlineManageSystem/5-LocalStorageDataConstruction',
+          children: [
+            '/'
+          ]
+        },
+      ],
     },
   },
   configureWebpack: {
---
+++ docs/Projects/VenueOnlineManageSystem/1-ComponentConstruction/README.md
@@ -0,0 +1,9 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-02-09 18:50:38
+ * @LastEditTime: 2021-02-09 18:50:39
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /vuepress-starter/docs/Projects/README.md
+-->
+# 123
