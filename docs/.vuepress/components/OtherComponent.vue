c68b395904b3bf2bc8c2c2c88b888a1f9edfb971
Evan Zuo <v_wangxiangbo01@baidu.com>
2021-11-07 19:27:25 +0800

    feat: dayjs

---
+++ docs/.vuepress/components/OtherComponent.vue
@@ -0,0 +1,68 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-01-24 20:09:22
+ * @LastEditTime: 2021-11-07 17:59:34
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /vuepress-starter/docs/.vuepress/components/home.vue
+-->
+<style scoped lang="sass">
+.app_container
+
+</style>
+
+<style lang="sass">
+
+</style>
+
+<template lang="pug">
+.app_container
+  .title 与茜茜宝贝的纪念日
+  .second {{ second }}
+  .minutes {{ minutes }}
+  .day {{ day }}
+  .months {{ months }}
+  .sum {{ sum }}
+
+</template>
+
+<script>
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import dayjs from 'dayjs'
+
+@Component({
+  created() {
+    setInterval(() => {
+      this.countTime()
+    }, 1000);
+  },
+
+  mounted() {
+  },
+
+  computed: {
+    sum() {
+      return this.second
+    }
+  }
+})
+
+class OtherComponent extends Vue {
+    second = ''
+    minutes = ''
+    day = ''
+    months = ''
+
+    countTime() {
+      const beginLoveTime = dayjs('2021-08-29 21:41:00')
+      const date2 = dayjs()
+      this.second = date2.diff(beginLoveTime, 'second')
+      this.minutes = date2.diff(beginLoveTime, 'minutes')
+      this.day = date2.diff(beginLoveTime, 'day')
+      this.months = date2.diff(beginLoveTime, 'months')
+    }
+}
+
+export default OtherComponent
+</script>
---
+++ docs/me.md
@@ -0,0 +1,9 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-11-07 17:48:32
+ * @LastEditTime: 2021-11-07 17:53:15
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /MyBlog/docs/me.md
+-->
+<OtherComponent />
